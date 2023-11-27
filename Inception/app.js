c618ed98e5828c5aa58f7e90e1ac0912ce5d76e2
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-11-27 12:56:10 +0000

    starting with react introduction p1

---
+++ Inception/app.js
@@ -0,0 +1,22 @@
+const header = React.createElement(
+  "h1",
+  { id: "heading" },
+  "HelloWorld! using React!"
+);
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm the first header"),
+    React.createElement("h1", {}, "I'm the second header"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm the first header in second child"),
+    React.createElement("h1", {}, "I'm the second header in second child"),
+  ])]
+);
+
+root.render(parent);

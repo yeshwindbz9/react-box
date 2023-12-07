46e595ff88f7d1b14bc52ba586d5efd3d119683a
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-12-07 16:56:10 +0000

    fixed swiggy api issue and CORS, added load more data

--- FoodinService/src/components/Body.js
+++ FoodinService/src/components/Body.js
@@ -2,7 +2,14 @@ import ResCard from "./ResCard";
 import Shimmer from "./Shimmer";
 import { useState, useEffect } from "react";
 import resData from "../utils/mockData";
-import { SWIGGY_API_URL } from "../utils/constants";
+import {
+  SWIGGY_API_URL,
+  SWIGGY_POST_DATA,
+  SWIGGY_POST_URL,
+} from "../utils/constants";
+
+//variable to update data
+let resCount = 10;
 
 const Body = () => {
   // normal variable (updates but does not rerender UI)
@@ -26,8 +33,37 @@ const Body = () => {
     const json = await data.json();
     // optional chaining
     let list =
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants;
+    setList(list);
+    setFiltered(list);
+  };
+
+  const updateData = async () => {
+    let data = SWIGGY_POST_DATA;
+    data.widgetOffset.collectionV5RestaurantListWidget_SimRestoRelevance_food_seo =
+      String(resCount);
+    resCount += 20;
+    console.log(resCount);
+    const response = await fetch(SWIGGY_POST_URL, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    const json = await response.json();
+    // optional chaining
+    let list =
+      json?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    list = resList.concat(list);
     setList(list);
     setFiltered(list);
   };
@@ -76,6 +112,15 @@ const Body = () => {
           Show All Places
         </button>
 
+        <button
+          className="filter-btn"
+          onClick={() => {
+            updateData();
+          }}
+        >
+          Load More Places
+        </button>
+
         <input
           placeholder="Search for a place 🔍"
           className="search-box"
@@ -93,7 +138,7 @@ const Body = () => {
         {filteredList.length === 0
           ? Array(20)
               .fill(1)
-              .map(() => <Shimmer />)
+              .map((e, i) => <Shimmer key={i} />)
           : filteredList.map((restaurant) => (
               <ResCard key={restaurant.info.id} {...restaurant} />
             ))}
--- FoodinService/src/utils/constants.js
+++ FoodinService/src/utils/constants.js
@@ -7,6 +7,31 @@ export const CART_ICON =
   "https://img.icons8.com/3d-fluency/94/shopping-cart.png";
 
 export const SWIGGY_API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0772926&lng=72.8541923&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const SWIGGY_POST_URL =
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/update";
+export const SWIGGY_POST_DATA = {
+  filters: {},
+  lat: 19.0772926,
+  lng: 72.8541923,
+  nextOffset: "COVCELQ4KIDY0bewgan2KjCnEzgE",
+  page_type: "DESKTOP_WEB_LISTING",
+  seoParams: {
+    apiName: "FoodHomePage",
+    pageType: "FOOD_HOMEPAGE",
+    seoUrl: "https://www.swiggy.com/",
+  },
+  widgetOffset: {
+    NewListingView_Topical_Fullbleed: "",
+    NewListingView_category_bar_chicletranking_TwoRows: "",
+    NewListingView_category_bar_chicletranking_TwoRows_Rendition: "",
+    Restaurant_Group_WebView_PB_Theme: "",
+    Restaurant_Group_WebView_SEO_PB_Theme: "",
+    collectionV5RestaurantListWidget_SimRestoRelevance_food_seo: "10",
+    inlineFacetFilter: "",
+    restaurantCountWidget: "",
+  },
+  _csrf: "q75CvAize3P2-XWl0fh-CZja-XVS-IzdsYcxahwo",
+};
